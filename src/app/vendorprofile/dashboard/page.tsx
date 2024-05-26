306381ae3a550a17a70d8b5febb73fb540af1372
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2024-05-27 00:13:33 +0100

    feat: vendorsProfileDashBoard_UI

--- src/app/vendorprofile/dashboard/page.tsx
+++ src/app/vendorprofile/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useRef, useState, useEffect, useCallback } from "react";
-import { Suspense } from 'react'
+import { Suspense } from "react";
 import Loading from "@/app/register/logo";
 import VendorNav from "@/components/nav/vendorNav/nav";
 import { useRouter } from "next/navigation";
@@ -18,8 +18,8 @@ import {
 } from "firebase/firestore";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import Firebase from "@/firebase/firebase";
-import {useAuthState} from "react-firebase-hooks/auth"
-import {useCollectionData} from "react-firebase-hooks/firestore"
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 import { Services } from "@/database/data";
 import styles from "./styles.module.css";
 
@@ -43,20 +43,45 @@ type FormValue = {
 const { auth, storage, database } = Firebase;
 
 export default function Profile() {
-	
 	const [profileDetails, setProfileDetails] = useState<FormValue | null>(null);
 
 	const [imageUrl, setImageUrl] = useState("");
 
 	const [tab, setTab] = useState("");
+	const [userA, setUser] = useState<any>(auth);
 
 	const [user, loading, error] = useAuthState(auth);
 
 	const router = useRouter();
-	
-	
 
+	onAuthStateChanged(auth, (user) => {
+		if (user) {
+			getDownloadURL(imageRef).then((url) => {
+				setImageUrl(url);
+			});
 
+			const handleGetProfileDetail = async () => {
+				try {
+					const querySnapshot = await getDocs(userQuery);
+
+					if (querySnapshot.empty) {
+						console.log("No profile details found");
+						return;
+					}
+
+					const retrievedData = querySnapshot.docs[0].data() as FormValue;
+					setProfileDetails(retrievedData);
+				} catch (error) {
+					console.error("Error getting profile detail:", error);
+				}
+			};
+
+			handleGetProfileDetail();
+		} else {
+			// Redirect to login page if not signed in
+			router.push("/");
+		}
+	});
 
 	const imageRef = ref(storage, `image/${user?.email}`);
 
@@ -68,16 +93,15 @@ export default function Profile() {
 			const file = fileInput.files[0]; // Get the first selected file
 
 			// Reference to the root of the default Firebase Storage bucket
-			
 
 			// Upload the file
 			uploadBytes(imageRef, file)
 				.then((snapshot) => {
-					getDownloadURL(imageRef).then((url)=>{
+					getDownloadURL(imageRef).then((url) => {
 						setImageUrl(url);
 						setTab("");
-					})
-					
+					});
+
 					console.log("Uploaded a file!");
 				})
 				.catch((error) => {
@@ -86,34 +110,12 @@ export default function Profile() {
 		}
 	};
 
-	const profileDetailRef = collection(
-		database,
-		`profile`
-	);
+	const profileDetailRef = collection(database, `profile`);
 
 	const userQuery = query(
 		profileDetailRef,
 		where("email", "==", `${user?.email}`)
-		);
-
-
-	const handleGetProfileDetail = async () => {
-
-		try {
-		
-			const querySnapshot = await getDocs(userQuery);
-
-			if (querySnapshot.empty) {
-				console.log("No profile details found");
-				return;
-			}
-
-			const retrievedData = querySnapshot.docs[0].data() as FormValue;
-			setProfileDetails(retrievedData);
-		} catch (error) {
-			console.error("Error getting profile detail:", error);
-		}
-	};
+	);
 
 	const categoryName = profileDetails?.selectCategory;
 	const serviceName = profileDetails?.selectService;
@@ -129,17 +131,16 @@ export default function Profile() {
 	const categoryImg = Category?.src;
 	const serviceImg = Service?.src;
 
-	
 	const handleProfileDetail = async () => {
 		try {
-			const profileDetailRef = collection(
-				database,
-				`profile`,
-
+			const profileDetailRef = collection(database, `profile`);
+			await setDoc(
+				doc(profileDetailRef, profileDetails?.docid),
+				{
+					src: imageUrl,
+				},
+				{ merge: true }
 			);
-			await setDoc(doc(profileDetailRef, profileDetails?.docid), {
-				src: imageUrl,
-			},{merge: true});
 			console.log("Profile detail added successfully");
 		} catch (error) {
 			console.error("Error adding profile detail:", error);
@@ -147,194 +148,236 @@ export default function Profile() {
 	};
 	useEffect(() => {
 		handleProfileDetail();
-	},[handleProfileDetail]);
-		
-	
-	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, (user) => {
-			if (user) {
-			
-
-				getDownloadURL(imageRef).then(url =>{
-					setImageUrl(url);
-				})
-
-				handleGetProfileDetail(); 
-				
-			} else {
-			// Redirect to login page if not signed in
-				router.push('/');
-			}
-		});
-	
-		// Cleanup function to avoid memory leaks
-		return () => unsubscribe();
-	}, [handleGetProfileDetail,imageRef,router]); // Re-run useEffect when imageUrl or auth changes
-useEffect(()=>{
-	auth.currentUser?.reload();
-},[handleGetProfileDetail,imageRef,imageUrl,router]);
-
-	
+	}, [handleProfileDetail]);
 
 	return (
-		<Suspense fallback={<Loading/>}>
-		<main  className={styles.mainBodyCover}>
-			<VendorNav/>
-			{loading && <Loading />}
-			{user && (<>
-			<div className={styles.mainBody}>
-			<div className={styles.profilePictureFlexControl}>
-				<div className={styles.profilePictureServiceCover}>
-					<div className={styles.serviceImgCover}>
-						<Image
-							object-fit="cover"
-							className={styles.serviceImg}
-							alt="Picture of the author"
-							quality={100}
-							width={100}
-							height={100}
-							src={`${serviceImg}`}
-							priority={true}
-							unoptimized
-						/>
-					</div>
-					<div className={styles.profilePictureBodyCover}>
-						<div className={styles.profilePictureBodyFlexControl}>
-							<div className={styles.profilePictureCover}>
-								<div className={styles.profileName}>{user?.displayName}</div>
-								{tab !== "update" && (
-									<>
-										<div className={styles.profileImgCover}>
+		<Suspense fallback={<Loading />}>
+			<main className={styles.mainBodyCover}>
+				<VendorNav />
+				{loading && <Loading />}
+
+				{user && (
+					<>
+						{" "}
+						<div className={styles.pageBodyCover}>
+							<div className={styles.bodyCover}>
+								<div className={styles.profilePictureFlexControl}>
+									<div className={styles.profilePictureServiceCover}>
+										<div className={styles.serviceImgCover}>
 											<Image
 												object-fit="cover"
-												className={styles.profileImg}
+												className={styles.serviceImg}
 												alt="Picture of the author"
 												quality={100}
 												width={100}
 												height={100}
-												src={`${imageUrl}`}
+												src={`${serviceImg}`}
 												priority={true}
 												unoptimized
 											/>
 										</div>
-										<div className={styles.updateCover}>
-											<span
-												onClick={() =>
-													tab === "update" ? setTab("") : setTab("update")
-												}
-												className={styles.clickToUpdate}
-											>
-												Update Profile picture
-											</span>
-										</div>
-									</>
-								)}
+										<div className={styles.profilePictureBodyCover}>
+											<div className={styles.profilePictureBodyFlexControl}>
+												<div className={styles.profilePictureCover}>
+													<div className={styles.profileName}>
+														{user?.displayName}
+													</div>
+													{tab !== "update" && (
+														<>
+															<div className={styles.profileImgCover}>
+																<Image
+																	object-fit="cover"
+																	className={styles.profileImg}
+																	alt="Picture of the author"
+																	quality={100}
+																	width={100}
+																	height={100}
+																	src={`${imageUrl}`}
+																	priority={true}
+																	unoptimized
+																/>
+															</div>
+															<div className={styles.updateCover}>
+																<span
+																	onClick={() =>
+																		tab === "update"
+																			? setTab("")
+																			: setTab("update")
+																	}
+																	className={styles.clickToUpdate}
+																>
+																	Update Profile picture
+																</span>
+															</div>
+														</>
+													)}
 
-								{tab === "update" && (
-									<div className={styles.formContainer}>
-										<div className={styles.inputImageCover}>
-											<input
-												type="file"
-												accept="image/*"
-												className={styles.input}
-												ref={fileInputRef}
-												placeholder="Upload Display Picture"
+													{tab === "update" && (
+														<div className={styles.formContainer}>
+															<div className={styles.inputImageCover}>
+																<input
+																	type="file"
+																	accept="image/*"
+																	className={styles.input}
+																	ref={fileInputRef}
+																	placeholder="Upload Display Picture"
+																/>
+															</div>
+															<button onClick={handleUpload}>Upload</button>
+															<button onClick={() => setTab("")}>Back</button>
+														</div>
+													)}
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div className={styles.form}>
+									<div className={styles.profileBodyCategoryCover}>
+										<div className={styles.profileBodyCover}>
+											<div className={styles.flexControl}>
+												<div className={styles.categoryCover}>
+													<div className={styles.formImgCover}>
+														<Image
+															object-fit="cover"
+															className={styles.detailImg}
+															alt="Picture of the author"
+															quality={100}
+															width={100}
+															height={100}
+															src={`${categoryImg}`}
+															priority={true}
+															unoptimized
+														/>
+													</div>
+													<div className={styles.firstcategory}>
+														<div className={styles.flexFirstcategory}>
+															<div className={styles.infoHeaderContainer}>
+																<span className={styles.titleHeaderInfo}>
+																	{profileDetails?.selectCategory} {"Service"}
+																</span>
+															</div>
+															<div className={styles.profileInfoContainer}>
+																<div className={styles.infoContainer}>
+																	<span className={styles.title}>
+																		Specialty{" "}
+																	</span>
+																	<span className={styles.titleInfo}>
+																		{profileDetails?.selectService}{" "}
+																		{"Specialist"}
+																	</span>
+																</div>
+																<div className={styles.infoContainer}>
+																	<span className={styles.title}>Country </span>
+																	<span className={styles.titleInfo}>
+																		{profileDetails?.countrySelect}
+																	</span>
+																</div>
+																<div className={styles.infoContainer}>
+																	<span className={styles.title}>State </span>
+																	<span className={styles.titleInfo}>
+																		{profileDetails?.stateSelect}
+																	</span>
+																</div>
+																<div className={styles.infoContainer}>
+																	<span className={styles.title}>Area </span>
+																	<span className={styles.titleInfo}>
+																		{profileDetails?.areaSelect}
+																	</span>
+																</div>
+																<div className={styles.infoContainer}>
+																	<span className={styles.title}>Address </span>
+																	<span className={styles.titleInfo}>
+																		{profileDetails?.address}
+																	</span>
+																</div>
+																<div className={styles.infoContainer}>
+																	<span className={styles.title}>Contact </span>
+																	<span className={styles.titleInfo}>
+																		{profileDetails?.number}
+																	</span>
+																</div>
+																<div className={styles.infoContainer}>
+																	<span className={styles.title}>Email </span>
+																	<span className={styles.titleInfoE}>
+																		{profileDetails?.email}
+																	</span>
+																</div>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div className={styles.midBody}>
+									<div className={styles.categoryCover}>
+										<div className={styles.categoryImgCover}>
+											<Image
+												object-fit="cover"
+												className={styles.categoryImg}
+												alt="Picture of the author"
+												quality={100}
+												width={100}
+												height={100}
+												src="/service/commerce.jpg"
+												priority={true}
+												unoptimized
 											/>
 										</div>
-										<button onClick={handleUpload}>
-											Upload
-										</button>
-										<button onClick={()=> setTab("")}>Back</button>
+										<div className={styles.SpaceEnter}>
+											{" "}
+											<div className={styles.openShop}>
+												{" "}
+												<p className={styles.openShopP}>Enter</p>
+											</div>
+										</div>
 									</div>
-								)}
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className={styles.profileBodyFlexControl}>
-				<div className={styles.profileBodyCategoryCover}>
-					
-				
-					<div className={styles.profileBodyCover}>
-					<div className={styles.flexControl}>
-					<div className={styles.categoryCover}>
-						<div className={styles.categoryImgCover}>
-            <Image
-							object-fit="cover"
-							className={styles.categoryImg}
-							alt="Picture of the author"
-							quality={100}
-							width={100}
-							height={100}
-							src={`${categoryImg}`}
-							priority={true}
-							unoptimized
-						/>
-						</div>
-						<div className={styles.categoryName}>
-							<div className={styles.flexCategoryName}>
-							<div className={styles.infoHeaderContainer}>
-									<span className={styles.titleHeaderInfo}>
-										{profileDetails?.selectCategory}{" "}{"Service"}
-									</span>
+									<div className={styles.mySpaceCover}>
+										<div className={styles.mySpace}>
+											<div className={styles.left}>{`Item's`}</div>
+											<div className={styles.right}>{`Shop's`}</div>
+										</div>
+										<div className={styles.mySpaceRender}></div>
+									</div>
 								</div>
-              <div className={styles.profileInfoContainer}>
-								
-								<div className={styles.infoContainer}>
-									<span className={styles.title}>Specialty </span>
-									<span className={styles.titleInfo}>
-										{profileDetails?.selectService}{" "}{"Specialist"}
-									</span>
-								</div>
-								<div className={styles.infoContainer}>
-									<span className={styles.title}>Country </span>
-									<span className={styles.titleInfo}>
-										{profileDetails?.countrySelect}
-									</span>
-								</div>
-								<div className={styles.infoContainer}>
-									<span className={styles.title}>State </span>
-									<span className={styles.titleInfo}>
-										{profileDetails?.stateSelect}
-									</span>
-								</div>
-								<div className={styles.infoContainer}>
-									<span className={styles.title}>Area </span>
-									<span className={styles.titleInfo}>
-										{profileDetails?.areaSelect}
-									</span>
-								</div>
-								<div className={styles.infoContainer}>
-									<span className={styles.title}>Address </span>
-									<span className={styles.titleInfo}>
-										{profileDetails?.address}
-									</span>
-								</div>
-								<div className={styles.infoContainer}>
-									<span className={styles.title}>Contact </span>
-									<span className={styles.titleInfo}>
-										{profileDetails?.number}
-									</span>
-								</div>
-								<div className={styles.infoContainer}>
-									<span className={styles.title}>Email </span>
-									<span className={styles.titleInfoE}>
-									{profileDetails?.email}
-									</span>
+								<div className={styles.body}>
+									<div className={styles.categoryCover}>
+										<div className={styles.categoryImgCover}>
+											<Image
+												object-fit="cover"
+												className={styles.categoryImg}
+												alt="Picture of the author"
+												quality={100}
+												width={100}
+												height={100}
+												src="/service/vacancy.jpg"
+												priority={true}
+												unoptimized
+											/>
+										</div>
+										<div className={styles.SpaceEnter}>
+											{" "}
+											<div className={styles.openShop}>
+												{" "}
+												<p className={styles.openShopP}>Enter</p>
+											</div>
+										</div>
+									</div>
+									<div className={styles.mySpaceCover}>
+										<div className={styles.mySpace}>
+											<div className={styles.left}>{`Job's`}</div>
+											<div className={styles.right}>{`Companies`}</div>
+										</div>
+										<div className={styles.mySpaceRender}></div>
+									</div>
 								</div>
 							</div>
-							</div>
 						</div>
-					</div>
-				</div>
-				
-				</div>
-				</div>
-			</div>
-			</div>
-			</>)}
-		</main>
+					</>
+				)}
+			</main>
 		</Suspense>
 	);
 }
