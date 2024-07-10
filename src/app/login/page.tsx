b3acf178af470eeb4d290919e0d6ff39d5e3e1b0
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2024-07-10 09:31:05 +0100

    style:LogIn

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -233,7 +233,7 @@ export default function Register() {
 														fill="none"
 														viewBox="0 0 24 24"
 														strokeWidth={1.5}
-														stroke="currentColor"
+														stroke="grey"
 														className={styles.svg}
 													>
 														<path
