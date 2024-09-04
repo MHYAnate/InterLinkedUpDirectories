8925f3f622d1398b3c9d4410b9adcdeda92cf2a7
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2024-09-04 20:37:35 +0100

    style:colorPalette

---
+++ src/components/btn/contactBtn.tsx
@@ -0,0 +1,39 @@
+import styles from "./styles.module.css";
+
+const ChatImageBtn: React.FC<any> = () => {
+	return (
+		<div className={styles.chatCoverBtnC}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				strokeWidth={1.5}
+				stroke="currentColor"
+				className={styles.svgSizeC}
+			>
+				 <path strokeLinecap="round" strokeLinejoin="round" d="m9 13.5 3 3m0 0 3-3m-3 3v-6m1.06-4.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+			</svg>
+
+  
+
+
+      <svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				strokeWidth={1.5}
+				stroke="currentColor"
+				className={styles.svgSizeC}
+			>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+				/>
+			</svg>
+		</div>
+	);
+};
+
+ChatImageBtn.displayName = "ChatImageBtn";
+export default ChatImageBtn;
