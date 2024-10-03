40d2868bdf949451f4a5c2133502490ce34b2fb0
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2024-10-03 20:24:31 +0100

    vendorsSelectWidthFix

--- src/components/chat/communityComponent.tsx
+++ src/components/chat/communityComponent.tsx
@@ -3,20 +3,73 @@ import React from "react";
 import Image from "next/image";
 import styles from "./styles.module.css";
 
-
-interface Community {
-  community:string;
-
+interface CommunityProps {
+	country: string;
+	state: string;
+	area: string;
+	category: string;
+	service: string;
+	setRoomName: any;
+	setRoomState: any;
+	setRoomLocation: any;
+	setContactNameDisplay: any;
+	setContactContactDisplay: any;
 }
 
-const CommunityComponent: React.FC<Community> = ({
-
-
+const CommunityComponent: React.FC<CommunityProps> = ({
+	country,
+	state,
+	area,
+	category,
+	service,
+	setRoomName,
+	setRoomState,
+	setRoomLocation,
+	setContactNameDisplay,
+	setContactContactDisplay,
 }) => {
-
 	return (
 		<div className={styles.communityCover}>
-      
+			<div className={styles.communityHolder}>
+				<div className={styles.communitySvg}></div>
+				<div className={styles.communityName}></div>
+			</div>
+			<div className={styles.communityHolder}>
+				<div className={styles.communitySvg}></div>
+				<div className={styles.communityName}></div>
+			</div>
+			<div className={styles.communityHolder}>
+				<div className={styles.communitySvg}></div>
+				<div className={styles.communityName}></div>
+			</div>
+			{category !== ( "" || null || undefined ) && (
+				<>
+					<div className={styles.communityHolder}>
+						<div className={styles.communitySvg}></div>
+						<div className={styles.communityName}></div>
+					</div>
+					<div className={styles.communityHolder}>
+						<div className={styles.communitySvg}></div>
+						<div className={styles.communityName}></div>
+					</div>
+					<div className={styles.communityHolder}>
+						<div className={styles.communitySvg}></div>
+						<div className={styles.communityName}></div>
+					</div>
+					<div className={styles.communityHolder}>
+						<div className={styles.communitySvg}></div>
+						<div className={styles.communityName}></div>
+					</div>
+					<div className={styles.communityHolder}>
+						<div className={styles.communitySvg}></div>
+						<div className={styles.communityName}></div>
+					</div>
+					<div className={styles.communityHolder}>
+						<div className={styles.communitySvg}></div>
+						<div className={styles.communityName}></div>
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
