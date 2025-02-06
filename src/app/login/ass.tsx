2a6e10bfda09fd6c3ace46b7eb55596f564bcb22
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2025-02-06 21:20:45 +0100

    fitBtnLogin

--- src/app/login/ass.tsx
+++ src/app/login/ass.tsx
@@ -153,7 +153,7 @@ export default function LoginPage() {
               </button>
 
               <p className="text-center text-sm text-gray-600">
-                Don't have an account?{" "}
+                {`Don't have an account?`}{" "}
                 <Link href="/register" className="font-medium text-green-600 hover:text-green-500">
                   Sign up
                 </Link>
